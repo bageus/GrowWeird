ed10ffc5ac1285d1abf80f90231e2a27e8a33688
bageus <36338626+bageus@users.noreply.github.com>
2026-08-12 20:52:21 +0200

    Add fertilizer offer weight

--- src/domain/definitions/fertilizer_definition.gd
+++ src/domain/definitions/fertilizer_definition.gd
@@ -4,6 +4,7 @@ extends Resource
 @export var id: StringName
 @export var display_name_key: String = ""
 @export var shop_price: int = 0
+@export_range(0.0, 100.0, 0.1) var offer_weight: float = 1.0
 
 # Hidden from the player. Keys are mutation axes, values are accumulated pressure.
 @export var mutation_contributions: Dictionary = {}
