392558d2695121101742fe44750c7c009bffe84f
bageus <36338626+bageus@users.noreply.github.com>
2026-08-12 20:06:43 +0200

    Harden persisted light mode typing

--- src/domain/state/pot_state.gd
+++ src/domain/state/pot_state.gd
@@ -12,7 +12,7 @@ const LIGHT_VALUES: Array[float] = [0.05, 0.4, 0.75, 1.0]
 
 var pot_id: String = ""
 var soil_moisture: float = 0.5
-var light_mode: LightMode = LightMode.DIFFUSED
+var light_mode: int = LightMode.DIFFUSED
 var window_open: bool = false
 var plant: PlantState
 
@@ -20,4 +20,4 @@ func is_empty() -> bool:
 	return plant == null
 
 func light_level() -> float:
-	return LIGHT_VALUES[int(light_mode)]
+	return LIGHT_VALUES[clampi(light_mode, 0, LIGHT_VALUES.size() - 1)]
