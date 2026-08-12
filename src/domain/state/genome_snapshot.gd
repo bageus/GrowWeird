ffb9324c49c77b4e697c1288b73739599f894239
bageus <36338626+bageus@users.noreply.github.com>
2026-08-12 20:46:01 +0200

    Preserve branch traits in genome snapshots

--- src/domain/state/genome_snapshot.gd
+++ src/domain/state/genome_snapshot.gd
@@ -4,12 +4,14 @@ extends RefCounted
 var species_id: StringName
 var ancestry: Array[String] = []
 var traits: Dictionary = {}
+var branch_traits: Dictionary = {}
 
 func duplicate_snapshot() -> GenomeSnapshot:
 	var copy := GenomeSnapshot.new()
 	copy.species_id = species_id
 	copy.ancestry = ancestry.duplicate()
 	copy.traits = traits.duplicate(true)
+	copy.branch_traits = branch_traits.duplicate(true)
 	return copy
 
 func is_empty() -> bool:
