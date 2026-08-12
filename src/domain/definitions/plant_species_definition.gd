fcd1c8dbed7a324f41e3583205f8834f8c950745
bageus <36338626+bageus@users.noreply.github.com>
2026-08-13 00:37:48 +0200

    Add lifecycle and silhouette tuning to species

--- src/domain/definitions/plant_species_definition.gd
+++ src/domain/definitions/plant_species_definition.gd
@@ -15,6 +15,14 @@ extends Resource
 @export_group("Growth")
 @export_range(0.00001, 0.1, 0.00001) var base_growth_per_second: float = 0.002
 
+@export_group("Lifecycle")
+@export_range(0.05, 0.6, 0.01) var sprout_stage_end: float = 0.18
+@export_range(0.2, 0.95, 0.01) var adult_growth_threshold: float = 0.65
+@export var native_regrowth_enabled: bool = true
+@export_range(1.0, 3600.0, 1.0) var native_regrowth_seconds: float = 120.0
+@export_range(0.0, 1.0, 0.01) var native_regrowth_min_health: float = 0.62
+@export_range(0.0, 1.0, 0.01) var native_regrowth_min_comfort: float = 0.62
+
 @export_group("Fruiting")
 @export_range(0.0, 1.0, 0.01) var fruiting_growth_threshold: float = 0.72
 @export_range(1.0, 3600.0, 1.0) var fruit_ripen_seconds: float = 90.0
@@ -29,3 +37,7 @@ extends Resource
 @export var wood_color: Color = Color(0.29, 0.18, 0.10)
 @export var leaf_color: Color = Color(0.22, 0.58, 0.24)
 @export var fruit_color: Color = Color(0.91, 0.42, 0.18)
+@export_range(0.5, 1.5, 0.01) var stem_height_scale: float = 1.0
+@export_range(0.5, 1.6, 0.01) var side_span_scale: float = 1.0
+@export_range(0.2, 1.6, 0.01) var side_lift_scale: float = 1.0
+@export_range(0.5, 1.6, 0.01) var leaf_scale: float = 1.0
