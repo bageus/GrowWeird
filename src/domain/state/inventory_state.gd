bb4538931ea0328540c3793b72caea57fcb8c1f1
bageus <36338626+bageus@users.noreply.github.com>
2026-08-12 20:45:29 +0200

    Add inventory state

---
+++ src/domain/state/inventory_state.gd
@@ -0,0 +1,7 @@
+class_name InventoryState
+extends RefCounted
+
+var fertilizers: Dictionary = {}
+var cuttings: Array[CuttingState] = []
+var seeds: Array[SeedState] = []
+var fruits: Array[FruitState] = []
