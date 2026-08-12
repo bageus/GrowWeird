7ad0c1102500ae8a75f038cf223bbac2a1c38af4
bageus <36338626+bageus@users.noreply.github.com>
2026-08-12 23:04:02 +0200

    Add species fruiting balance

--- src/domain/definitions/plant_species_definition.gd
+++ src/domain/definitions/plant_species_definition.gd
@@ -14,4 +14,11 @@ extends Resource
 
 @export_group("Growth")
 @export_range(0.00001, 0.1, 0.00001) var base_growth_per_second: float = 0.002
+
+@export_group("Fruiting")
+@export_range(0.0, 1.0, 0.01) var fruiting_growth_threshold: float = 0.72
+@export_range(1.0, 3600.0, 1.0) var fruit_ripen_seconds: float = 90.0
+@export var fruit_base_value: int = 35
+
+@export_group("Economy")
 @export var base_sale_value: int = 100
