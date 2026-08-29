c0454913908b090db559e6c3cf8ac131887cbcc5
bageus <36338626+bageus@users.noreply.github.com>
2026-08-30 00:20:52 +0300

    Make pot assets Ctrl-draggable and Ctrl-wheel scalable

--- src/presentation/main/pot_visual.gd
+++ src/presentation/main/pot_visual.gd
@@ -1,6 +1,7 @@
 class_name PotVisual
 extends Control
 
+const LAYOUT_PATH := "user://pot_asset_layout.cfg"
 const POT_TEXTURES := [
 	preload("res://assets/pot/pot_01.png"),
 	preload("res://assets/pot/pot_02.png"),
@@ -17,14 +18,71 @@ const GROUND_TEXTURES := {
 	"very_wet": preload("res://assets/pot/pot_ground/ground_03.png"),
 }
 
+@onready var stand: TextureRect = $Stand
 @onready var ground: TextureRect = $Ground
 @onready var pot: TextureRect = $Pot
+@onready var save_layout_button: Button = $SaveLayoutButton
+
+var _drag_target: Control
+var _drag_offset := Vector2.ZERO
+
+func _ready() -> void:
+	for node in _assets():
+		node.gui_input.connect(_on_asset_gui_input.bind(node))
+	save_layout_button.pressed.connect(save_asset_layout)
+	_load_asset_layout()
 
 func set_pot_state(state: PotState) -> void:
 	if state == null:
 		return
 	pot.texture = POT_TEXTURES[_pot_index(state.pot_id)]
 	ground.texture = GROUND_TEXTURES[_soil_key(state.soil_moisture)]
+
+func save_asset_layout() -> void:
+	var config := ConfigFile.new()
+	for node in _assets():
+		config.set_value("assets", node.name + "_position", node.position)
+		config.set_value("assets", node.name + "_scale", node.scale)
+	config.save(LAYOUT_PATH)
+
+func _load_asset_layout() -> void:
+	var config := ConfigFile.new()
+	if config.load(LAYOUT_PATH) != OK:
+		return
+	for node in _assets():
+		var saved_position := config.get_value("assets", node.name + "_position", node.position)
+		var saved_scale := config.get_value("assets", node.name + "_scale", node.scale)
+		if saved_position is Vector2:
+			node.position = saved_position
+		if saved_scale is Vector2:
+			node.scale = saved_scale
+
+func _assets() -> Array[Control]:
+	return [stand, ground, pot]
+
+func _on_asset_gui_input(event: InputEvent, target: Control) -> void:
+	if not Input.is_key_pressed(KEY_CTRL):
+		return
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				_drag_target = target
+				_drag_offset = target.get_global_mouse_position() - target.global_position
+				target.move_to_front()
+			elif _drag_target == target:
+				_drag_target = null
+		elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			_scale_asset(target, 1.1)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			_scale_asset(target, 0.9)
+	elif event is InputEventMouseMotion and _drag_target == target and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		target.global_position = target.get_global_mouse_position() - _drag_offset
+
+func _scale_asset(target: Control, factor: float) -> void:
+	var next_scale := target.scale * factor
+	next_scale.x = clampf(next_scale.x, 0.2, 4.0)
+	next_scale.y = clampf(next_scale.y, 0.2, 4.0)
+	target.scale = next_scale
 
 func _pot_index(pot_id: String) -> int:
 	var digits := ""
