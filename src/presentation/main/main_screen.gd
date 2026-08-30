b08baebbface577bbf2c86ad5b16a94a608e909d
bageus <36338626+bageus@users.noreply.github.com>
2026-08-30 15:01:50 +0300

    Keep main screen within quality line limit

--- src/presentation/main/main_screen.gd
+++ src/presentation/main/main_screen.gd
@@ -38,10 +38,6 @@ var _water_submenu_visible := false
 var _lighting_submenu_visible := false
 var _prune_cursor: Texture2D = null
 func _ready() -> void:
-	var tree_test := get_node_or_null("Shell/Layout/LeftSidebar/LeftScroll/LeftLayout/TreeGrowthControls")
-	var left_layout := get_node_or_null("Shell/Layout/LeftSidebar/LeftScroll/LeftLayout")
-	if tree_test != null and left_layout != null:
-		left_layout.move_child(tree_test, 4)
 	GameApp.state_changed.connect(_refresh)
 	GameApp.mutations_resolved.connect(_on_mutations_resolved)
 	GameApp.fertilizer_offer_ready.connect(_on_offer_ready)
