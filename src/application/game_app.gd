086b635cbb1e533209bbb358cb2c0c461e164c16
bageus <36338626+bageus@users.noreply.github.com>
2026-08-30 17:51:06 +0300

    Keep GameApp within authored line limit

--- src/application/game_app.gd
+++ src/application/game_app.gd
@@ -89,10 +89,8 @@ func water_active(use_sprayer: bool = false) -> bool:
 	var pot := active_pot()
 	if pot == null or pot.plant == null or not pot.plant.alive:
 		return false
-	if use_sprayer:
-		pot.soil_moisture = clampf(pot.soil_moisture + rules.sprayer_soil_amount, 0.0, 1.0)
-	else:
-		pot.moisten_soil_one_stage()
+	if use_sprayer: pot.soil_moisture = clampf(pot.soil_moisture + rules.sprayer_soil_amount, 0.0, 1.0)
+	else: pot.moisten_soil_one_stage()
 	_progress(&"watered")
 	state_changed.emit()
 	return true
