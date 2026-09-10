52967ef276f3c2dab750d259fc7e4e94af9f10a9
bageus <36338626+bageus@users.noreply.github.com>
2026-09-10 23:08:59 +0300

    Curve Energy banner title

--- src/presentation/main/energy_topup_panel.gd
+++ src/presentation/main/energy_topup_panel.gd
@@ -12,6 +12,7 @@ var _ad_pending := false
 func _ready() -> void:
 	(%Background as Panel).add_theme_stylebox_override(&"panel", UiAtlas.warm_hud_style(8, 26, Vector4(24.0, 20.0, 24.0, 22.0)))
 	%Banner.texture = UiAtlas.HUD_COIN_ENERGY_BANNER
+	CommerceUiStyle.curved_title($Window/MenuTitle, "ENERGY")
 	UiAtlas.configure_topup_card(%Energy5, "5 Energy", UiAtlas.energy_coin_icon(3, 3))
 	UiAtlas.configure_topup_card(%Energy15, "15 Energy", UiAtlas.energy_coin_icon(3, 2))
 	UiAtlas.configure_topup_card(%Energy30, "30 Energy", UiAtlas.energy_coin_icon(3, 4))
